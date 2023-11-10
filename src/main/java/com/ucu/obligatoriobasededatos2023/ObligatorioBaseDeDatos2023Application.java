b00fb1118357f25ee49a981028b17527ae15f819
tomasdarracq <juan.darracq@correo.ucu.edu.uy>
2023-11-10 20:52:56 -0300

    prueba

---
+++ src/main/java/com/ucu/obligatoriobasededatos2023/ObligatorioBaseDeDatos2023Application.java
@@ -0,0 +1,13 @@
+package com.ucu.obligatoriobasededatos2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatorioBaseDeDatos2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatorioBaseDeDatos2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucu/obligatoriobasededatos2023/ObligatorioBaseDeDatos2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucu.obligatoriobasededatos2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatorioBaseDeDatos2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
