ae218eb25672fd073f51f4ecb86840cb848ac459
tomasdarracq <juan.darracq@correo.ucu.edu.uy>
2023-11-14 09:12:38 -0300

    swagger

--- backend/src/main/java/com/ucu/obligatoriobasededatos2023/SpringFoxConfig.java
+++ backend/src/main/java/com/ucu/obligatoriobasededatos2023/SpringFoxConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SpringFoxConfig {
@@ -16,5 +17,7 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
-}
+
+    }
+
 }
