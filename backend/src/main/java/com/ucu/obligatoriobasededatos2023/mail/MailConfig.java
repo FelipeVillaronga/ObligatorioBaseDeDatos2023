601d6b601138b00528308cd93f3b054c7d8caf4b
tomasdarracq <juan.darracq@correo.ucu.edu.uy>
2023-11-16 20:23:46 -0300

    mail

--- backend/src/main/java/com/ucu/obligatoriobasededatos2023/mail/MailConfig.java
+++ backend/src/main/java/com/ucu/obligatoriobasededatos2023/mail/MailConfig.java
@@ -16,7 +16,7 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("obligatorio33@gmail.com");
-        mailSender.setPassword("obligatorioucu2023");
+        mailSender.setPassword("jzeitqbkacjldrwt");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
