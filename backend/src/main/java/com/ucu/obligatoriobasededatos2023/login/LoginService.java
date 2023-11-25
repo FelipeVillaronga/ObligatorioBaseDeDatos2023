b7a787d1a148a9f51af5fea36bf3954f68a38742
tomasdarracq <juan.darracq@correo.ucu.edu.uy>
2023-11-25 17:19:00 -0300

    login fix

--- backend/src/main/java/com/ucu/obligatoriobasededatos2023/login/LoginService.java
+++ backend/src/main/java/com/ucu/obligatoriobasededatos2023/login/LoginService.java
@@ -21,14 +21,17 @@ public class LoginService {
 
     public boolean addLogin(Login login) {
         String password = loginRepository.findLoginByLogId(login.getLogId());
+        System.out.println(password);
+        if (password == null) {
+            return false;
+        }
         return password.equals(login.getPassword());
     }
     public void addNewLogin(Login login) {
         try {
             loginRepository.save(login);
         } catch (DataIntegrityViolationException ex) {
-            // Manejar la excepción específica, por ejemplo, mostrar un mensaje de error.
-            // También puedes realizar alguna acción específica según tus necesidades.
+            
             System.err.println("Error: El usuario ya existe con el mismo LogId.");
         }
     }
